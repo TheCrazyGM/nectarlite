618496cc67891847c7732d356ec552ab962d7b86
Michael Garcia <thecrazygm@gmail.com>
2025-10-16 15:48:30 -0400

    refactor: migrate vote listener example to use AsyncApi instead of synchronous Api

--- examples/async_live_vote_listener.py
+++ examples/async_live_vote_listener.py
@@ -3,14 +3,15 @@
 import asyncio
 import logging
 
-from nectarlite import Api
+from nectarlite.api import AsyncApi
 from nectarlite.stream import AsyncStream
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 async def main():
-    api = Api(["https://api.hive.blog"])
+    api = AsyncApi(["https://api.hive.blog"])
     listener = AsyncStream(api=api, blockchain_mode="head")
 
     logging.info("Listening for new votes asynchronously... (Ctrl+C to stop)")
